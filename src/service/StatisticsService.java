4563296a5453e9be6157c8208be0d79304b71242
Mateusz Zięcina <ziecinamat+github@gmail.com>
2025-02-02 14:39:54 +0100

    Statistics commands adjustment

--- src/service/StatisticsService.java
+++ src/service/StatisticsService.java
@@ -2,15 +2,46 @@ package service;
 
 import model.CommandContext;
 import utils.StatisticsImageGenerator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StatisticsService {
-    
+    private static final Map<String, String> STATISTICS_VIEWS_MAP = new HashMap<>();
 
-     public static void handleStatsCommand(CommandContext context) {
+    static {
+        STATISTICS_VIEWS_MAP.put("colors", "Colors_statistic_view");
+        STATISTICS_VIEWS_MAP.put("games", "Games_statistics_view");
+        STATISTICS_VIEWS_MAP.put("global", "Global_statistics_view");
+        STATISTICS_VIEWS_MAP.put("lotto", "Lotto_statistics_view");
+        STATISTICS_VIEWS_MAP.put("main", "Main_global_statistics_view");
+        STATISTICS_VIEWS_MAP.put("mines", "Mines_statistic_view");
+        STATISTICS_VIEWS_MAP.put("race", "Race_statistic_view");
+        STATISTICS_VIEWS_MAP.put("roulette", "Roulette_statistic_view");
+        STATISTICS_VIEWS_MAP.put("slots", "Slots_statistic_view");
+    }
+
+    public static void handleStatsCommand(CommandContext context) {
         String userName = context.getUserName();
-        String statisticsType = context.getFirstArgument();
+        String statisticsType = context.getFirstArgument().trim().toLowerCase();
 
-        StatisticsImageGenerator.generateViewImage(statisticsType, userName);
+        String viewName = STATISTICS_VIEWS_MAP.get(statisticsType);
+
+        if (statisticsType == null || viewName == null) {
+            sendAvailableViewsMessage();
+            return;
+        }
+
+        StatisticsImageGenerator.generateViewImage(viewName, userName);
+
         MessageService.sendMessageFromClipboard(true);
-     }
+    }
+
+    private static void sendAvailableViewsMessage() {
+        StringBuilder message = new StringBuilder("Available statistics types: ");
+        for (String key : STATISTICS_VIEWS_MAP.keySet()) {
+            message.append(key).append(", ");
+        }
+        message.delete(message.length() - 2, message.length());
+        MessageService.sendMessage(message.toString());
+    }
 }
