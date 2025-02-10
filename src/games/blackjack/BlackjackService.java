e5e093c0343305b9fbf588185e0be07ae0dcf426
Mateusz Zięcina <ziecinamat+github@gmail.com>
2025-02-10 20:49:47 +0100

    Blackjack game - bj histo correction

--- src/games/blackjack/BlackjackService.java
+++ src/games/blackjack/BlackjackService.java
@@ -75,7 +75,7 @@ public class BlackjackService {
             userBalance = UserService.updateAndRetriveUserBalance(userName, winnings);
 
             String gameStatus = userName + " Blackjack! You won " + winnings + "!";
-            GameHistoryRepository.addGameHistory(userName, "Blackjack", context.getFullCommand(), betAmount, winnings, "Player hand: " + handToString(playerHand) + " Dealer hand: " + handToString(dealerHand));
+            GameHistoryRepository.addGameHistory(userName, "Blackjack", context.getFullCommand(), betAmount, winnings + betAmount, "Player hand: " + handToString(playerHand) + " Dealer hand: " + handToString(dealerHand));
             
             BlackjackImageGenerator.generateBlackjackImage(userName, playerHand, dealerHand, gameStatus, userBalance, betAmount, true, playerScore, dealerScore);
             MessageService.sendMessageFromClipboard(true);
