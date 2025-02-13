4b85f6d15c3b1ba4bb5506f701699a61cb3d00bb
Mateusz Zięcina <ziecinamat+github@gmail.com>
2025-02-13 22:09:07 +0100

    busy_timeout and journal_mode added to DatabaseConnectionManager to avoid writing to busy databse

--- src/database/DatabaseConnectionManager.java
+++ src/database/DatabaseConnectionManager.java
@@ -3,13 +3,14 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.io.File;
 
 public class DatabaseConnectionManager {
 
     private static Connection connection;
 
-    private static String databasePath = "src" + File.separator + "database" + File.separator + "mydatabase.db";
+    private static final String databasePath = "src" + File.separator + "database" + File.separator + "mydatabase.db";
     private static final String DATABASE_URL = "jdbc:sqlite:" + databasePath;
 
     private DatabaseConnectionManager() {
@@ -18,8 +19,16 @@ public class DatabaseConnectionManager {
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
             connection = DriverManager.getConnection(DATABASE_URL);
+            setupDatabase(connection);
         }
         return connection;
-    } 
-        
+    }
+
+    private static void setupDatabase(Connection conn) throws SQLException {
+        try (Statement stmt = conn.createStatement()) {
+
+            stmt.execute("PRAGMA busy_timeout = 5000;");
+            stmt.execute("PRAGMA journal_mode = WAL;");
+        }
+    }
 }
