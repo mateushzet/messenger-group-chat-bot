c7ededecc0025fd1129805a41f9e73ed0863fbc7
EmilLesniak <emilmaks0@gmail.com>
2025-02-19 15:07:03 +0100

    Added statistics for plinko on medium risk game

---
+++ src/database/scripts/Plinko_specific_medium_statistic_view.sql
@@ -0,0 +1,62 @@
+CREATE VIEW Plinko_specific_medium_statistic_view AS 
+SELECT 
+    main.user_name,
+    SUM(main.bet_amount) AS Total_chips_bet_on_plinko,
+    SUM(main.result_amount) AS Total_plinko_score,
+    COALESCE(winnings.Total_winnings_from_plinko, 0) AS Total_winnings_from_plinko,
+    COALESCE(losses.Total_chips_lost_on_plinko, 0) AS Total_chips_lost_on_plinko,
+    COUNT(main.id) AS Total_games_played_on_plinko,
+    AVG(main.bet_amount) AS Average_bet_amount_on_plinko,
+    MAX(main.result_amount) AS Maximum_win_on_plinko,
+    MIN(main.result_amount) AS Biggest_loss_on_plinko,
+    COALESCE(win_streak.Win_streak, 0) AS Win_streak,
+    COALESCE(lose_streak.Lose_streak, 0) AS Lose_streak,
+    COALESCE(hit_counts.Number_of_0x2_hits, 0) AS Number_of_0x2_hits,
+    COALESCE(hit_counts.Number_of_0x7_hits, 0) AS Number_of_0x7_hits,
+    COALESCE(hit_counts.Number_of_1x1_hits, 0) AS Number_of_1x1_hits,
+    COALESCE(hit_counts.Number_of_1x6_hits, 0) AS Number_of_1x6_hits,
+    COALESCE(hit_counts.Number_of_3x2_hits, 0) AS Number_of_3x2_hits,
+    COALESCE(hit_counts.Number_of_5x6_hits, 0) AS Number_of_5x6_hits,
+    COALESCE(hit_counts.Number_of_x12_hits, 0) AS Number_of_x12_hits,
+    COALESCE(hit_counts.Number_of_x60_hits, 0) AS Number_of_x60_hits
+FROM game_history AS main
+LEFT JOIN (
+    SELECT user_name, SUM(result_amount) AS Total_winnings_from_plinko
+    FROM game_history
+    WHERE result_amount > 0 AND game_type = 'Plinko' AND bet_command LIKE '%m%'
+    GROUP BY user_name
+) winnings ON main.user_name = winnings.user_name
+LEFT JOIN (
+    SELECT user_name, SUM(result_amount) AS Total_chips_lost_on_plinko
+    FROM game_history
+    WHERE result_amount < 0 AND game_type = 'Plinko' AND bet_command LIKE '%m%'
+    GROUP BY user_name
+) losses ON main.user_name = losses.user_name
+LEFT JOIN (
+    SELECT user_name, MAX(max_streak) AS Win_streak
+    FROM game_series
+    WHERE result_type = 1 AND game_type = 'Plinko'
+    GROUP BY user_name
+) win_streak ON main.user_name = win_streak.user_name
+LEFT JOIN (
+    SELECT user_name, MAX(max_streak) AS Lose_streak
+    FROM game_series
+    WHERE result_type = -1 AND game_type = 'Plinko'
+    GROUP BY user_name
+) lose_streak ON main.user_name = lose_streak.user_name
+LEFT JOIN (
+    SELECT user_name, 
+        COUNT(CASE WHEN note LIKE '0.2' THEN 1 END) AS Number_of_0x2_hits,
+        COUNT(CASE WHEN note LIKE '0.7' THEN 1 END) AS Number_of_0x7_hits,
+        COUNT(CASE WHEN note LIKE '1.1' THEN 1 END) AS Number_of_1x1_hits,
+        COUNT(CASE WHEN note LIKE '1.6' THEN 1 END) AS Number_of_1x6_hits,
+        COUNT(CASE WHEN note LIKE '3.2' THEN 1 END) AS Number_of_3x2_hits,
+        COUNT(CASE WHEN note LIKE '5.6' THEN 1 END) AS Number_of_5x6_hits,
+        COUNT(CASE WHEN note LIKE '12.0' THEN 1 END) AS Number_of_x12_hits,
+        COUNT(CASE WHEN note LIKE '60.0' THEN 1 END) AS Number_of_x60_hits
+    FROM game_history
+    WHERE game_type = 'Plinko' AND bet_command LIKE '%m%'
+    GROUP BY user_name
+) hit_counts ON main.user_name = hit_counts.user_name
+WHERE main.game_type = 'Plinko' AND main.bet_command LIKE '%m%'
+GROUP BY main.user_name;
